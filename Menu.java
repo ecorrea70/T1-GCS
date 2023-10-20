2670ef05fab45e0e7450a1471b37b3bc05fee617
ecorrea70 <eduardodasilvacorrea70@gmail.com>
2023-10-19 21:32:19 -0300

    criação da estrutura do menu

--- Menu.java
+++ Menu.java
@@ -1,4 +1,36 @@
 import java.util.Scanner;
 public class Menu {
-    Scanner entrada = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int opcao = 0;
+
+        while (opcao!=1){
+            menuInterativo();
+            opcao = entrada.nextInt();
+
+            switch (opcao){
+                case 1:
+                    System.out.println("Saindo...");
+                    break;
+                case 2:
+                    System.out.println("Cadastrando item...");
+                    break;
+                case 3:
+                    System.out.println("Cadastrando pedido...");
+                    break;
+            }
+
+        }
+
+    }
+
+    public static void menuInterativo(){
+        System.out.println("Selecione uma opção:");
+        System.out.println("1 - Sair");
+        System.out.println("2 - Cadastrar Item");
+        System.out.println("3 - Cadastrar Pedido");
+    }
 }
