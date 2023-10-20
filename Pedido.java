4393c5ff7a7f618f808d938d914b5091054960a9
Leonardo <silvaleonardoduarte2222@gmail.com>
2023-10-20 17:06:07 -0300

    Commit com alterações da classe 'pedido'

--- Pedido.java
+++ Pedido.java
@@ -1,22 +1,17 @@
 import java.util.ArrayList;
+import java.util.Date;
+import java.time.LocalDate;
+import java.util.Locale;
+
 public class Pedido {
     private int codigoPedido; //Não especifica mas precisa de um pra poder buscar o pedido depois
     private String funcionario; // saber qual o funcionario
     private String departamento; // esse aqui vai receber o departamento do funcionario de cima
-    private int dataPedido;
-    private int dataConclusao;
-    private StatusPedido status;
-    private ArrayList<Item> listaDePedidos;
-
-     public Pedido(int codigoPedido, Usuario usuario, String departamento, int dataPedido, int dataConclusao, StatusPedido status) {
-        this.codigoPedido = codigoPedido;
-        this.funcionario = usuario.getNome();
-        this.departamento = departamento;
-        this.dataPedido = dataPedido;
-        this.dataConclusao = dataConclusao;
-        this.status = status;
-        listaDePedidos = new ArrayList<>();
-    }
+    private LocalDate dataPedido;
+    //    private Date dataConclusao;
+    private String status;
+    private ArrayList<Item> itens;
+    private ArrayList<Pedido> pedidos;
 
     public int getCodigoPedido() {
         return codigoPedido;
@@ -42,31 +37,57 @@ public class Pedido {
         this.departamento = departamento;
     }
 
-    public int getDataPedido() {
+    public LocalDate getDataPedido() {
         return dataPedido;
     }
 
-    public void setDataPedido(int dataPedido) {
+    public void setDataPedido(LocalDate dataPedido) {
         this.dataPedido = dataPedido;
     }
 
-    public int getDataConclusao() {
-        return dataConclusao;
-    }
+//    public Date getDataConclusao() {
+//        return dataConclusao;
+//    }
+//
+//    public void setDataConclusao(Date dataConclusao) {
+//        this.dataConclusao = dataConclusao;
+//    }
 
-    public void setDataConclusao(int dataConclusao) {
-        this.dataConclusao = dataConclusao;
-    }
-
-    public StatusPedido getStatus() {
+    public String getStatus() {
         return status;
     }
 
-    public void setStatus(StatusPedido status) {
+    public void setStatus(String status) {
         this.status = status;
     }
-    
-    public void adicionarItem(Item i){
-        listaDePedidos.add(i);
+
+    public ArrayList<Item> getItens() {
+        return itens;
     }
+
+    public void setItens(ArrayList<Item> itens) {
+        this.itens = itens;
+    }
+
+    public Pedido(int codigoPedido, String funcionario, String departamento, LocalDate dataPedido/* Date dataConclusao*/, String status, ArrayList<Item> itens) {
+        this.codigoPedido = codigoPedido;
+        this.funcionario = funcionario;
+        this.departamento = departamento;
+        this.dataPedido = dataPedido;
+//        this.dataConclusao = dataConclusao;
+        this.status = "aberto";
+        this.itens = itens;
+    }
+
+//    public void concluiPedido(){ // Para poder concluir o pedido e pegar a data atual dele
+//        dataConclusao = new Date();
+//    }
+//    public void adicionarPedido(Pedido pedido){
+//       pedidos.add(pedido);
+//        System.out.println(pedido.codigoPedido + pedido.itens.get(0).getQuantidade());
+//    }
+
+
+
 }
+
